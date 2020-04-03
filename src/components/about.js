4a656e041cd3676ec029882b813fb001e4e53619
Jackie Allex <jackieallex@Jackies-MacBook-Pro.local>
2020-04-03 11:28:20 -0400

    Add three.js model to scene

--- src/components/about.js
+++ src/components/about.js
@@ -4,16 +4,15 @@ import { Grid, Cell } from 'react-mdl';
 import './about.css';
 import Flexbox from 'flexbox-react';
 import { Element } from 'react-scroll'
+import pic from '../images/about/about.jpg';
 
 class About extends Component {
   render() {
     return(
-      <section className="aboutText"  id="about" style={{  
-        backgroundImage: "url(" + "https://images.pexels.com/photos/34153/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" + ")",
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat'
-      }} alignItems="center">
+      <section className="aboutText"  id="about" alignItems="center">
+         <div className="logo">
+          <img src={pic}/>
+        </div>
           <h3>Hi! I'm Jackie Allex and I'm a developer and animator. </h3>
           </section>
     )
--- src/components/home.js
+++ src/components/home.js
@@ -1,19 +1,61 @@
 import React, { Component } from 'react';
-import { Grid, Cell } from 'react-mdl';
+
 import './home.css';
+import * as THREE from "three";
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 class Home extends Component {
+  
+  componentDidMount() {
+      var scene = new THREE.Scene();
+      scene.background = new THREE.Color(0xC9886D);
+      var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+  
+      var width = 100;
+      var height = 100;
+      var intensity = 1.4;
+      var rectLight = new THREE.RectAreaLight( 0xffffff, intensity,  width, height );
+      rectLight.position.set( 1, 1, 10 );
+      rectLight.lookAt( 1, 1, 3 );
+      scene.add( rectLight )
+  
+      let renderer = new THREE.WebGLRenderer({antialias: true});
+      renderer.setSize( window.innerWidth, window.innerHeight );
+      document.getElementsByClassName("image")[0].appendChild( renderer.domElement );
+  
+  
+      camera.position.z = 5;
+      var animate = function () {
+        requestAnimationFrame( animate );
+        renderer.render( scene, camera );
+      };
+  
+      let loader = new GLTFLoader();
+      loader.load(
+        "/orange-model.glb",
+        ( gltf ) => {
+            // called when the resource is loaded
+          console.log(gltf.scene)
+          scene.add(gltf.scene);
+        },
+        ( xhr ) => {
+          console.log(xhr);
+          // called while loading is progressing
+          // console.log("The xhr warning isL ",xhr.srcElement.responseText);
+      }
+        );
+  
+      animate();
+  
+  }
+
   render() {
     return(
-      <div style={{width: '100%', margin: 0, padding: 0}} className="homeText" id="home" alignItems="center">
+      <div style={{width: '100%', margin: 0, padding: 0}} className="homeText" id="home" alignitems="center">
           <div className="home-grid">
             <div className="image">
-            <img
-              src="https://p7.hiclipart.com/preview/961/21/288/low-poly-fruit-art-rendering-3d-modeling-diamond-perspective-mango.jpg"
-              alt="3dModel"
-              className="model-img"
-              />
-              </div>
+            <div ref={ref => (this.mount = ref)} />
+            </div>
             <div className="banner-text">
               <h1>Jackie Allex</h1>
               <div className="social-links">
